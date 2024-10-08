810cb5daceb5116f7289abd76afb8cfe4695f5d9
Conor Koritor <ckoritor@gmail.com>
2024-10-08 19:36:26 +0100

    Worksheet 3 Complete (dotenv not implemented as it seems to break everything)

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,5 @@
 import express, {Router} from "express";
+import {authenticateKey} from "../middleware/auth.middleware";
 import{
     getUsers,
     getUserById,
@@ -11,7 +12,7 @@ const router: Router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
-router.post('/', createUser);
+router.post('/', authenticateKey, createUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
 
