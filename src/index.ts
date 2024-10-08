197e4276ecd297b8a3ab378c4d148fb4729f6647
Conor Koritor <ckoritor@gmail.com>
2024-10-08 19:26:05 +0100

    Got rid of dotenv for now as it seems to be messing with the authenticate key

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-/*import express, {Application, Request, Response} from "express" ;
+import express, {Application, Request, Response} from "express" ;
 import morgan from "morgan";
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 
 //internal imports
 import userRoutes from "./routes/users";
 import {authenticateKey} from './middleware/auth.middleware';
 
-dotenv.config();
+//dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,7 +14,7 @@ const app: Application = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(authenticateKey);
+//app.use(authenticateKey);
 
 app.get("/ping", async (_req : Request, res: Response) => {    
     res.send({
@@ -38,44 +38,4 @@ app.use('/api/v1/users', authenticateKey, userRoutes);
 
  app.listen(PORT, () => {
     console.log("Server is running on port  --", PORT);
-    });*/
-
-/* external imports */
-
-import express, {Application, Request, Response} from "express" ;
-
-import morgan from "morgan";
-
-/* internal imports */
-
-import userRoutes from './routes/users';
-import {authenticateKey} from './middleware/auth.middleware';
-
-const PORT = process.env.PORT || 3000;
-
-const app: Application = express();
-
-
-
-app.use(morgan("tiny"));
-app.use(express.json());
-
-app.use(authenticateKey); 
-
-app.get("/ping", async (_req : Request, res: Response) => {
-    res.json({
-    message: "hello from Una - has this changed",
     });
-});
-
-
-app.get('/bananas', async (_req : Request, res: Response) =>
-  res.send('hello world, this is bananas - ha ha ha ha hda ha '));
-
-app.use('/api/v1/users', authenticateKey, userRoutes)
-
-
-app.listen(PORT, () => {
-    console.log("Server is running on port  --", PORT);
-    });
-
