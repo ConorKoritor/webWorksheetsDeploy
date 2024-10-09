554145aa7fd07a1b568a61f31fbdf0597b3ab1f1
Conor Koritor <ckoritor@gmail.com>
2024-10-09 10:21:42 +0100

    Implemented GradeHistory Model and controller

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import { MongoClient, Db, Collection}  from "mongodb";
 import  dotenv from "dotenv";
 import User from './models/user'
+import { GradeHistory } from "./models/gradeHistories";
 
 
   dotenv.config();
@@ -10,15 +11,18 @@ import User from './models/user'
 
 let db : Db 
 export let usersCollection : Collection<User>
+export let gradeHistoriesCollection : Collection<GradeHistory>
+export const collections: { users?: Collection<User>, gradeHistories?: Collection<GradeHistory> } = {};
 
-export const collections: { users?: Collection<User> } = {};
 
 client.connect().then
 (()=>
   {
   db = client.db(dbName);
   usersCollection  = db.collection('users');
+  gradeHistoriesCollection = db.collection('grades');
   collections.users = usersCollection;
+  collections.gradeHistories = gradeHistoriesCollection;
   console.log('Connected to database');
 }
 )
