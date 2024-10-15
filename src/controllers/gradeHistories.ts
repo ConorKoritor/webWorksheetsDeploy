299a9dcdfb9fe9fe9f418ede5a99830e624e619e
Conor Koritor <ckoritor@gmail.com>
2024-10-15 23:40:12 +0100

    Finished Task 4

--- src/controllers/gradeHistories.ts
+++ src/controllers/gradeHistories.ts
@@ -12,11 +12,14 @@ export const getGradeHistories = async (req: Request, res: Response) => {
       const gradeHistories = (await gradeHistoriesCollection
         .find(filterObj)
         //Filters to only show the student_id and class_id fields
-        //.project({'student_id': 1, 'class_id': 1 })
-        
-        //Filters to not show the scores field student_id field
-        .project({'student_id': 0, 'scores.score': 0 })
+        //Change 1s to 0s to exclude those fields and show the rest
+        .project({'student_id': 1, 'class_id': 1 })
+        //Sorts by Ascending class_id
+        //Change 1 to -1 to sort by Descending class_id
+        .sort({'class_id': 1})
+
         .toArray()) as GradeHistory[];
+
       res.status(200).json(gradeHistories);
     }
     catch(error){
